d6032289c63ebfe981ab969bde059753dd34021a
MedvedKIEV1997 <burdakigor@gmail.com>
2022-02-23 20:57:48 +0200

    separating app in client and express server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import CRWNswg from "../../assets/crown.svg";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51KS3gKKypKh6bkHDOC4GJUdtpe1y2WHbs3AdS4fVHSR81nZaeCJWscyRD8Ut8kZAkNs1j9hVRbfvIc6bv1XAORqn00oigpEOG8";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert("Payment successful");
+      })
+      .catch((err) => {
+        console.log("Payment error: ", JSON.parse(err));
+        alert("There was an issue with your payment");
+      });
   };
   return (
     <StripeCheckout
